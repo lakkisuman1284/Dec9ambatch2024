f71f95dc061aeedd4eab54f2d43de50b0185cef6
lakkisuman1284 <lakki.suman85@gmail.com>
2024-12-19 17:24:45 +0530

    added code

--- src/com/rameshsoft/automation/appln/businessscripts/GitHardwork.java
+++ src/com/rameshsoft/automation/appln/businessscripts/GitHardwork.java
@@ -1,5 +1,10 @@
 package com.rameshsoft.automation.appln.businessscripts;
 
 public class GitHardwork {
-
+public void subject() {
+	System.out.println("subject first job next");
+	System.out.println("subject first job next");
+	System.out.println("subject first job next");
+	System.out.println("subject first job next");
+}
 }
--- src/com/rameshsoft/automation/appln/businessscripts/GmailTest.java
+++ src/com/rameshsoft/automation/appln/businessscripts/GmailTest.java
@@ -21,6 +21,8 @@ public class GmailTest extends BaseTest {
 		nxt.click();
 		System.out.println("java");
 		System.out.println("java");
+		System.out.println("java");
+		System.out.println("java");
 	}
 	
 }
