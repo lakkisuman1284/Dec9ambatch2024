3ea439915a6a0cbfcd2bd97842977576a13530b5
lakkisuman1284 <lakki.suman85@gmail.com>
2024-12-19 22:00:33 +0530

    code is added

---
+++ src/com/rameshsoft/automation/appln/businessscripts/GitHardwork1.java
@@ -0,0 +1,12 @@
+package com.rameshsoft.automation.appln.businessscripts;
+
+public class GitHardwork {
+public void subject() {
+	System.out.println("subject first job next");
+	System.out.println("subject first job next");
+	System.out.println("subject first jobs next");
+	System.out.println("subject first job next");
+	System.out.println("subject first job next");
+	System.out.println("subject firstt job nextt");
+}
+}
